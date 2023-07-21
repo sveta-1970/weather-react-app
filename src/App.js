a3f8f34d44656d6c6d671511c72c52ee3d23e956
sveta-1970 <ssarisoydan@gmail.com>
2023-07-22 00:33:27 +0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by{" "}
+        <a
+          href="https://www.linkedin.com/in/svitlana-shkriabina-5a2072157/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Svitlana Shkriabina
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/sveta-1970/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
