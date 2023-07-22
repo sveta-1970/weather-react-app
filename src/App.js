649afc592be74c4655941dc86a126d49674f7449
sveta-1970 <ssarisoydan@gmail.com>
2023-07-22 13:05:57 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was created by{" "}
-        <a
-          href="https://www.linkedin.com/in/svitlana-shkriabina-5a2072157/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Svitlana Shkriabina
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/sveta-1970/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://www.linkedin.com/in/svitlana-shkriabina-5a2072157/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Svitlana Shkriabina
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/sveta-1970/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
