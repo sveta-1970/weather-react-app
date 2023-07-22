89fd8a61eca03a17c1fc2628a8e416bdef2a3313
sveta-1970 <ssarisoydan@gmail.com>
2023-07-22 17:21:45 +0300

    Added css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was created by{" "}
           <a
