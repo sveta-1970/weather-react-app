0dd149a67041a0cdcdde64f39806167c32325ad3
sveta-1970 <ssarisoydan@gmail.com>
2023-07-29 14:21:29 +0300

    Last  updates

--- src/Weather.js
+++ src/Weather.js
@@ -10,7 +10,6 @@ export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
-    console.log(response.data);
     setWeatherData({
       ready: true,
       coord: response.data.coord,
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,9 +15,17 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
-  if (loaded) {
-    console.log(forecast);
 
+  function load() {
+    let apiKey = "2980ff43226d67e53abfcdb6d457dcc8";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
@@ -28,18 +36,15 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "2980ff43226d67e53abfcdb6d457dcc8";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
+    load();
     return null;
   }
 }
